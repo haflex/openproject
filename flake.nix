fc2edf2bc84aa4fdeb369a7d223780990cc4f0bb
Mattis Hasler <mail@pendor.de>
2023-06-13 19:31:55 +0000

    rewrite most of the build stuff

--- flake.nix
+++ flake.nix
@@ -12,65 +12,82 @@
       pkgs = nixpkgs.legacyPackages.${system};
       name = "OpenProject";
       gems = ruby-nix.lib pkgs {
-        inherit name;
+        name = "${name}-Gems";
         gemset = nix/gemset.nix;
         ruby = pkgs.ruby_3_2;
       };
       version = "12.5.4";
-    in
-    {
-      packages.backend = pkgs.stdenvNoCC.mkDerivation {
-        pname = "${name}-Backend";
+      inherit (pkgs) postgresql;
+
+      openproject = pkgs.buildNpmPackage {
+        pname = name;
         inherit version;
-
-        buildInputs = [
-          gems.env
-        ];
-
-        src = ./.;
-
-        patches = [
-          nix/rake_no_spring.patch
-        ];
-
-        installPhase = ''
-          mkdir $out
-          cp -r app bin config db extra lib lib_static modules spec $out
-          cp Gemfile Gemfile.lock Gemfile.modules config.ru Rakefile $out
-        '';
-          #cp -r * $out
-      };
-      packages.frontend = pkgs.buildNpmPackage {
-        pname = "${name}-Frontend";
-        inherit version;
-
-        srcs = [
-          ./frontend
-          ./modules
-        ];
-        sourceRoot = "frontend";
 
         nativeBuildInputs = [
+          gems.env
           pkgs.python3
-          pkgs.nodejs
         ];
 
-        preBuild = ''
-          mkdir -p ../public/assets/
-          cp src/app/features/plugins/linked-plugins.module.ts.example src/app/features/plugins/linked-plugins.module.ts
-          cp src/app/features/plugins/linked-plugins.styles.sass.example src/app/features/plugins/linked-plugins.styles.sass
+        src = self;
+        sourceRoot = "source/frontend";
+
+        prePatch = ''
+          cd ..
+          chmod u+w bin
+          chmod u+w lib/tasks
         '';
 
-        postInstall = ''
-          cp -r ../public $out/
+        postPatch = ''
+          cd frontend
         '';
 
-        #dontNpmBuild = true;
+        #patch -p1 < nix/rake_no_spring.patch
+        #patch lib/tasks/assets.rake nix/assets_no_npm.patch
+        patches = [
+          nix/rake_no_spring.patch
+          nix/assets_no_npm.patch
+        ];
 
         npmDepsHash = "sha256-cGrgMwhh/WfahMd8TbzHZ6PruU+4V7cogWJp8gMCIlI=";
 
+        preBuild = ''
+          cd ..
+          patchShebangs --build bin
+          mkdir ../nulldb
+          mkdir ../pgsock
+          pwd
+          ls
+          chmod u+w log/ tmp/ public/ config/
+          ${postgresql}/bin/initdb -D ../nulldb --auth=trust
+          ${postgresql}/bin/pg_ctl -D ../nulldb -l ../pglog -o '-c listen_addresses="" -c unix_socket_directories=$PWD/../pgsock' -w start
+          ${postgresql}/bin/createdb -h /build/pgsock assets
+          ${postgresql}/bin/createuser -h /build/pgsock postgres
+          SECRET_KEY_BASE=dfg876adfhg9768ah DATABASE_URL=postgres:///assets?host=/build/pgsock RAILS_ENV=production bin/rake db:migrate assets:precompile
+          ${postgresql}/bin/pg_ctl -D ../nulldb stop
+          cd frontend
+        '';
+
+        postBuild = ''
+          cd ..
+          SECRET_KEY_BASE=dfg876adfhg9768ah DATABASE_URL=postgres:///assets?host=/build/pgsock RAILS_ENV=production bin/rake assets:rebuild_manifest
+          cd frontend
+        '';
+        #${postgresql}/bin/psql -h /build/pgsock -U postgres -c "grant all privileges on database assets to postgres;"
+
+        installPhase = ''
+          cd ..
+          mkdir $out
+          cp -r app bin config db extra lib lib_static modules spec $out
+          cp Gemfile Gemfile.lock Gemfile.modules config.ru Rakefile $out
+          cp -r public $out
+        '';
+          #cp -r * $out
       };
-    
+    in {
+      packages = {
+        inherit openproject;
+        default = openproject;
+      };
       devShells.default = pkgs.mkShell {
         packages = with pkgs; [
           gems.env
@@ -79,6 +96,11 @@
       devShells.bundix = pkgs.mkShell {
         packages = with pkgs; [
           bundix
+        ];
+      };
+      devShells.postgres = pkgs.mkShell {
+        packages = with pkgs; [
+          postgresql
         ];
       };
     }
